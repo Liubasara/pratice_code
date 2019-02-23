213747ac03eeff401c5d0c7361b89667e21f4895
Liu <followliu@163.com>
2019-02-24 00:04:18 +0800

    Update myBind.js

--- javascript/myBind.js
+++ javascript/myBind.js
@@ -1,7 +1,11 @@
 Function.prototype.myBind = function (context) {
   if (typeof context !== 'function') throw new TypeError('类型错误')
   const args = [...arguments].slice(1)
-  return function () {
-    return this.call(context)
+  var _this = this
+  return function F () {
+    if (this instanceof F) {
+      return new _this(...args, ...arguments)
+    }
+    return _this.apply(context, args.concat(...arguments))
   }
 }
