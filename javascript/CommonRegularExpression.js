578ed5b2a95827da6932f9e62f3b45a7f9baa605
Liu <followliu@163.com>
2019-04-22 16:56:53 +0800

    Update CommonRegularExpression.js

--- javascript/CommonRegularExpression.js
+++ javascript/CommonRegularExpression.js
@@ -5,3 +5,16 @@ var percent = /^(100.0(0)?|100|[1-9]?\d(\.\d\d?)?)%$|^0$|^0.00$/
 
 // 任意正整数，正小数（小数位不超过2位）
 var isNum=/^(([1-9][0-9]*)|(([0]\.\d{1,2}|[1-9][0-9]*\.\d{1,2})))$/
+
+// js给数字加三位一逗号间隔
+function format_number(nStr ){
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];  
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    let rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {  
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }  
+    return x1 + x2;  
+}
