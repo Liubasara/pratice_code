cd16f68a0d66bb95b9572b7847bb7ea9eb1b511e
liubasara <followliu@163.com>
2020-08-18 01:53:34 +0800

    update 剪绳子贪心算法

--- 算法/动态规划/剪绳子.js
+++ 算法/动态规划/剪绳子.js
@@ -14,7 +14,7 @@
  * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
  */
 
- /**
+ /** 动态规划 O(N)
  * @param {number} n
  * @return {number}
  */
@@ -46,8 +46,29 @@ function cuttingRope (n) {
     }
     dp[i] = max
   }
-  console.log(dp)
   return dp[n]
+}
+
+/** 贪婪算法 O(1) 可轻松用于大数相乘
+* https://www.cnblogs.com/zhuangzi101/p/11774309.html
+* 根据数学证明可得知，每一段分出来的长度越接近自然对数 e，所得到的总乘积就会越大
+* 而 e 的大小约为 2.71828，而题目要求分为整数，所以要做的就是尽可能地把线段分成多的长度为 3 的子段，余下的分为 2
+* 唯一一个例外是当余下长度为 4 的时候，因为 2*2 > 3*1，所以应该将绳子减为 2 的两段
+* @param {number} n
+* @return {number}
+*/
+function cuttingRope2 (n) {
+  if (n < 2) return 0
+  if (n === 2) return 1
+  if (n === 3) return 2
+  let split3Times = Math.floor(n / 3) // 有多少条长度为 3 的线段
+  if (n % 3 === 1) {
+    // 尽可能获得长度为 3 的线段后，若余下长度为 1，则说明有倒数第二段时余下的线段长度为 4，所以应返回最后一段留待 2 整除
+    split3Times--
+  }
+  let split2Times = Math.floor((n - (split3Times * 3)) / 2)
+  // PS: 由于 JavaScript 精度问题，使用 Math.pow 计算大数的时候会遇到精度丧失导致计算错误无法 AC 的问题
+  return (Math.pow(3, split3Times) * Math.pow(2, split2Times)) % 1000000007
 }
 
 console.log(cuttingRope(10))
