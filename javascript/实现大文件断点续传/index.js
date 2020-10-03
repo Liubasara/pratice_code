9288cb3ceffb22254f09ea4dbfea0352b6a40392
liubasara <followliu@163.com>
2020-10-03 14:14:05 +0800

    update client端

--- javascript/实现大文件断点续传/index.js
+++ javascript/实现大文件断点续传/index.js
@@ -15,8 +15,25 @@ server.get('', function (req, res) {
   res.sendFile(path.resolve('src', './index.html'))
 })
 
+server.post('/hashCheck', function (req, res) {
+  // TODO: 校验文件 hash 值，返回 type
+  // 返回数据中 res.data.type == 2(文件已上传过) 1(断点续传) 0(从未上传)
+  res.status(200).send({
+    data: {
+      type: 1,
+      index: [88]
+    }
+  })
+})
+
 server.post('/upload', function (req, res) {
+  // TODO: 上传接口，将分片文件保存在服务器
   res.sendStatus(201)
+})
+
+server.post('/mergeChunks', function (req, res) {
+  // TODO: 等所有上传完毕后进行合并
+  res.sendStatus(200)
 })
 
 const client = server.listen(3000, function () {
--- javascript/实现大文件断点续传/src/assets/Utils.js
+++ javascript/实现大文件断点续传/src/assets/Utils.js
@@ -1,3 +1,7 @@
+/**
+ * myAjax 请求包装
+ * @param {*} opt 
+ */
 export const myAjax = function (
   opt = {
     url: '',
@@ -39,4 +43,44 @@ export const myAjax = function (
         throw Error('myAjax 暂不支持此 method')
     }
   })
+}
+
+/**
+ * 限制 Promise 并发
+ * @param {Array<PromiseConstructor>} todoList 
+ * @param {number} limit 
+ */
+export const limitPromise = (todoList, limit) => {
+  const todoLen = todoList.length
+  const lockFn = []
+  const result = []
+  return new Promise((wattingResolve) => {
+    function wrapFnToPromise (fn, index) {
+      function dealLcokFn (res) {
+        result.push(res)
+        lockFn.splice(index, 1)
+        if (todoList.length > 0) {
+          lockFn.push(wrapFnToPromise(todoList.shift(), lockFn.length))
+        } else if (result.length === todoLen) {
+          wattingResolve(result)
+        }
+      }
+      // 将方法再包一层，每一个 promise 任务完成时触发，将列表中下一个任务(如果有的话)放入任务列表中并执行，同时将自己清除出当前列表
+      return new Promise((resolve, reject) => {
+        fn().then(res => {
+          dealLcokFn(res)
+          resolve(res)
+        }).catch(e => {
+          dealLcokFn(e)
+          reject(e)
+        })
+      })
+    }
+    // 最多只有 limit 个任务，且每个任务完成后都会主动拉下一个列表，保证同时执行的任务永远有且只有 limit 个
+    for (let i = 0; i < limit; i++) {
+      lockFn.push(wrapFnToPromise(todoList[i], i))
+    }
+    todoList.splice(0, limit)
+  })
+  
 }
