fcbe0bbed0839449d35897c77990ae1c4bcc608a
liubasara <followliu@163.com>
2021-02-16 23:18:50 +0800

    判断移动端pc端并进行屏蔽跳转案例

--- javascript/判断移动端pc端.js
+++ javascript/判断移动端pc端.js
@@ -1,4 +1,4 @@
-// 将该脚本放在 index.html 脚本的最上面
+// 判断移动端pc端并进行屏蔽跳转 将该脚本放在 index.html 脚本的最上面
 let system = {}
 let p = navigator.platform
 let u = navigator.userAgent
@@ -15,3 +15,11 @@ if (system.win || system.mac || system.xll) {
     window.location.href = '/default.html'
   }
 }
+
+/**
+ * 测试适用网址：http://api.xosxx.com/hls/playA.php?vid=qGfJeztPsOO0OO1hMNBiltxhIv8GQszrbV1sNDMWuE84HwmweVvL0jmDOO0OO9x7Xdh319ir
+ * 
+ * userAgent 修改（可以借助插件或者 devtools 的 NetworkCondition 进行 userAgent 的修改）: Mozilla/5.0 (iPhone; CPU Windows Phone) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 EdgiOS/44.5.0.10 Mobile/15E148 Safari/604.1
+ * platform 修改: Object.defineProperty(navigator,'platform',{get:function(){return 'Android';}});
+ * location.href = 'http://api.xosxx.com/hls/playA.php?vid=qGfJeztPsOO0OO1hMNBiltxhIv8GQszrbV1sNDMWuE84HwmweVvL0jmDOO0OO9x7Xdh319ir'
+ */
