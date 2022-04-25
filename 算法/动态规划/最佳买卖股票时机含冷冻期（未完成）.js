41a3ed854cdcd388e0d4ba757135a89ba7354be5
liubasara <followliu@163.com>
2022-04-26 00:27:19 +0800

    2022年04月26日  0:27:19

--- 算法/动态规划/最佳买卖股票时机含冷冻期（未完成）.js
+++ 算法/动态规划/最佳买卖股票时机含冷冻期（未完成）.js
@@ -24,6 +24,27 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-  
+var maxProfit = function (prices) {
+  const pricesLen = prices.length
+  if (pricesLen <= 1) return 0;
+  const dp = []
+  dp[0] = []
+  // 0: 持有股票  1: 未持有股票，处于冷冻期 2: 未持有股票，未处于冷冻期
+  // dp[i] 代表第i天
+  dp[0][0] = -prices[0]
+  dp[0][1] = 0
+  dp[0][2] = 0
+  for (let i = 1; i < pricesLen; i++) {
+    const priceI = prices[i]
+    dp[i] = []
+    // 持有股票，说明要么是昨天买了股票 f[i-1][0] , 要么今天买了股票（那么第 i-1 天就不能持有股票并且不能处于冷冻期中，对应的状态为 f[i-1][2] 加上买入股票的负收益 -priceI）
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - priceI)
+    // 没股票且处于冷冻期, 说明是在今天把股票卖了，收益与昨天的持有状态 dp[i - 1][0] 叠加
+    dp[i][1] = dp[i - 1][0] + priceI
+    // 没股票且处于非冷冻期, 说明昨天没有买入，今天没有卖出，最大收益是昨天操作结果的最大值
+    dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][2])
+  }
+  return Math.max(dp[pricesLen - 1][1], dp[pricesLen - 1][2])
 };
+
+console.log(maxProfit([1, 2, 3, 0, 2])) // 3
