f9e78d7fe2ef0b0379b924c68a90cd31c464c6fd
liubasara <followliu@163.com>
2022-05-10 23:01:17 +0800

    2022年05月10日 23:01:17

--- 算法/链表/反转链表.js
+++ 算法/链表/反转链表.js
@@ -3,7 +3,7 @@ function ListNode(val) {
   this.next = null
 }
 
-function createNodeChainByList (arr) {
+function createNodeChainByList(arr) {
   if (!arr[0] && arr[0] !== 0) return
   let firstNode = new ListNode(arr[0])
   let tmpNode = firstNode
@@ -17,16 +17,17 @@ function createNodeChainByList (arr) {
 
 var firstNode = createNodeChainByList([1, 2, 3, 4, 5])
 // while (firstNode) {
-  // console.log(firstNode.val)
-  // firstNode = firstNode.next
+// console.log(firstNode.val)
+// firstNode = firstNode.next
 // }
 
 /**
   反转链表
 **/
-function reverseChain (node) {
+var reverseList = function (head) {
+  if (!head) return null
   let lastNode
-  function dfs (node) {
+  function dfs(node) {
     let tmp = node
     if (tmp.next) {
       dfs(tmp.next) // 递归进入下一个节点
@@ -37,33 +38,39 @@ function reverseChain (node) {
       lastNode = tmp
     }
   }
-  dfs(node)
-  node.next = null // 原来的头结点现在是尾结点，指针需要指向 null
+  dfs(head)
+  head.next = null // 原来的头结点现在是尾结点，指针需要指向 null
   return lastNode
 }
 
-var reverseFirstNode = reverseChain(firstNode)
+// var reverseFirstNode = reverseList(firstNode)
+
+// while (reverseFirstNode) {
+//   console.log(reverseFirstNode.val)
+//   reverseFirstNode = reverseFirstNode.next
+// }
+
+/**
+  非递归反转链表（三指针法）
+  题解：http://c.biancheng.net/view/8105.html
+**/
+var reverseList = function traverseReversList(head) {
+  if (!head) return null
+  let p1 = head
+  let p2 = head.next
+  while (p2) {
+    let p3 = p2.next
+    p2.next = p1
+    p1 = p2
+    p2 = p3
+  }
+  head.next = null
+  return p1
+}
+
+var reverseFirstNode = reverseList(firstNode)
 
 while (reverseFirstNode) {
   console.log(reverseFirstNode.val)
   reverseFirstNode = reverseFirstNode.next
-}
-
-/**
-  非递归反转链表
-**/
-function traverseReverseChain (node) {
-  const stack = []
-  let tmp = node
-  while (!!tmp) {
-    stack.push(tmp)
-    tmp = tmp.next
-  }
-  const newFirstNode = stack[stack.length - 1]
-  while (stack.length > 0) {
-    const lastNode = stack.pop()
-    lastNode.next = stack[stack.length - 1]
-  }
-  node.next = null
-  return newFirstNode
 }
