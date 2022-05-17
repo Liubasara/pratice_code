ce7de05d2b2d382a22b4eca8117fae70f359e936
liubasara <followliu@163.com>
2022-05-18 00:24:11 +0800

    2022年05月18日  0:24:11

--- 算法/数组应用/三数求和（对撞指针经典题目）.js
+++ 算法/数组应用/三数求和（对撞指针经典题目）.js
@@ -11,56 +11,80 @@
   示例： 给定数组 nums = [-1, 0, 1, 2, -1, -4]， 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
 */
 
-function threeSum (nums) {
-  // 先对数组进行升序排序
-  nums = nums.sort(function (a, b) {
-    return a - b
-  })
-  const result = []
-  const len = nums.length - 1
-  for (let i = 0;i < len - 2;i++) {
-    let j = i + 1
-    let k = len
-    // 跳过重复的数据
-    if (i > 0 && nums[i] === nums[i - 1]) {
-      continue
+// 题解：https://leetcode.cn/problems/3sum/solution/pai-xu-shuang-zhi-zhen-zhu-xing-jie-shi-python3-by/
+
+function threeSum(nums) {
+  function quickSort(arr) {
+    const arrLen = arr.length
+    function start(left = 0, right = arrLen - 1) {
+      let p1 = left
+      let p2 = right
+      let pointIdx = Math.floor((left + right) / 2)
+      const pointVal = arr[pointIdx]
+      while (p1 <= p2) {
+        while (arr[p1] < pointVal && p1 <= p2) {
+          p1++
+        }
+        while (arr[p2] > pointVal && p1 <= p2) {
+          p2--
+        }
+        if (p1 <= p2) {
+          ;[arr[p1], arr[p2]] = [arr[p2], arr[p1]]
+          p1++
+          p2--
+        }
+      }
+      if (p1 > left + 1) {
+        start(left, p1 - 1)
+      }
+      if (p1 < right) {
+        start(p1, right)
+      }
     }
-    while (j < k) {
-      if (nums[i] + nums[j] + nums[k] < 0) {
-        // 三数之和小于0，左指针前进
-        j++
-        while (nums[j] === nums[j - 1]) {
-          // 跳过重复的数据
-          j++
+    start()
+    return arr
+  }
+  // 快速排序 O(NlogN)
+  quickSort(nums)
+  const numsLen = nums.length
+  const res = []
+  for (let i = 0; i < numsLen; i++) {
+    const numI = nums[i]
+    // 若 nums[i]>0 因为已经排序好，所以后面不可能有三个数加等于 0，直接返回结果
+    if (numI > 0) return res
+    // 去重
+    if (i > 0 && numI === nums[i - 1]) continue
+    let p1 = i + 1
+    let p2 = numsLen - 1
+    // 双指针降格为两数求和
+    while (p1 < p2) {
+      const numP1 = nums[p1]
+      const numP2 = nums[p2]
+      if (numI + numP1 + numP2 === 0) {
+        res.push([numI, numP1, numP2])
+        while (p1 < p2 && nums[p1] === nums[p1 + 1]) {
+          p1++
         }
-        continue
-      }
-      if (nums[i] + nums[j] + nums[k] > 0) {
-        // 三数之和大于0，右指针后退
-        k--
-        while (nums[k] === nums[k + 1]) {
-          // 跳过重复的数据
-          k--
+        p1++
+        while (p1 < p2 && nums[p2] === nums[p2 - 1]) {
+          p2--
         }
-        continue
-      }
-      if (nums[i] + nums[j] + nums[k] === 0) {
-        // 三数之和等于0，入栈，左右指针变更
-        result.push([nums[i], nums[j], nums[k]])
-        j++
-        k--
-        while (nums[j] === nums[j - 1]) {
-          // 跳过重复的数据
-          j++
+        p2--
+      } else if (numI + numP1 + numP2 < 0) {
+        while (p1 < p2 && nums[p1] === nums[p1 + 1]) {
+          p1++
         }
-        while (nums[k] === nums[k + 1]) {
-          // 跳过重复的数据
-          k--
+        p1++
+      } else if (numI + numP1 + numP2 > 0) {
+        while (p1 < p2 && nums[p2] === nums[p2 - 1]) {
+          p2--
         }
+        p2--
       }
     }
   }
-  return result
+  return res
 }
 
-threeSum([-1, 0, 1, 2, -1, -4])
+// [ [-1, 0, 1], [-1, -1, 2] ]
+console.log(threeSum([-1, 0, 1, 2, -1, -4]))
