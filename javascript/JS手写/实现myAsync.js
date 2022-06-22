0b94ae710692296be80a99b85b847c8b081ae847
liubasara <followliu@163.com>
2022-06-23 01:46:38 +0800

    2022年06月23日  1:46:38

---
+++ javascript/JS手写/实现myAsync.js
@@ -0,0 +1,71 @@
+/**
+ * @param {() => Generator<Promise<any> | string, void, any>} fn 
+ */
+function myAsync(fn) {
+  return new Promise((resolve, reject) => {
+    const gen = fn()
+    function throwErr(err) {
+      let ret
+      try {
+        ret = gen.throw(err)
+      } catch (e) {
+        reject(e)
+      }
+      if (ret.done) {
+        return reject(ret.value)
+      }
+      Promise.resolve(ret.value).then(data => {
+        next(data)
+        return data
+      }, err => {
+        throwErr(err)
+        return err
+      })
+    }
+    function next(val) {
+      let ret
+      try {
+        ret = gen.next(val)
+      } catch (e) {
+        throwErr(e)
+      }
+      if (ret.done) {
+        return resolve(ret.value)
+      }
+      Promise.resolve(ret.value).then(data => {
+        next(data)
+        return data
+      }, err => {
+        throwErr(err)
+        return err
+      })
+    }
+    next()
+  })
+}
+
+
+myAsync(function* () {
+  console.log(
+    yield new Promise((resolve) => setTimeout(() => resolve(1), 1000))
+  )
+
+  console.log(
+    yield new Promise((resolve) => setTimeout(() => resolve(2), 2000))
+  )
+  try {
+    yield new Promise((resolve, reject) => setTimeout(() => reject(3), 3000))
+  } catch (e) {
+    console.log(e)
+  }
+
+  try {
+    yield new Promise((resolve, reject) => setTimeout(() => reject(4), 1000))
+  } catch (e1) {
+    console.log(e1)
+  }
+
+
+  console.log(yield 'all')
+  console.log('done')
+})
