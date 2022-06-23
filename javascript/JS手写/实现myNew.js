f2a2c343be08b71b5d8d10b1937ed3d68f31ac41
liubasara <followliu@163.com>
2022-06-23 23:42:00 +0800

    2022年06月23日 23:42:00

--- javascript/JS手写/实现myNew.js
+++ javascript/JS手写/实现myNew.js
@@ -1,6 +1,8 @@
 function myNew(fn, ...args) {
   if (typeof fn !== 'function') throw Error('myNew arg0 has not accept a function')
-  const obj = Object.create(fn.prototype) // 相当于 const obj = {};obj.__proto__ = fn.prototype
+  // 相当于 const obj = {};obj.__proto__ = fn.prototype
+  // 或是 ES6 的 const obj = Object.setPrototypeOf({}, fn.prototype)
+  const obj = Object.create(fn.prototype)
   const res = fn.apply(obj, args)
   return (typeof res === 'object' && res !== null) ? res : obj
 }
