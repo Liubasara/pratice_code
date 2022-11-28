0c132955c97dbc8e56ee6c19fb77a184b6ccaca6
Liu <followliu@163.com>
2022-11-28 19:44:38 +0800

    Create 调用input框或API选择文件.js

---
+++ javascript/调用input框或API选择文件.js
@@ -0,0 +1,22 @@
+// 兼容
+const openFile = async () => {
+  return new Promise((resolve) => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.addEventListener('change', () => {
+      resolve(input.files[0]);
+    });
+    input.click();
+  });
+};
+
+// chrome 特有 API
+const openFileByChromeAPI = async () => {
+  try {
+    // Always returns an array.
+    const [handle] = await window.showOpenFilePicker();
+    return handle.getFile();
+  } catch (err) {
+    console.error(err.name, err.message);
+  }
+};
