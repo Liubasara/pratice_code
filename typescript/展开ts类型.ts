b89460adbdc8109e1ade5ed6f2a799e76cdc48f1
Liu <followliu@163.com>
2023-11-16 11:18:42 +0800

    Create 展开ts类型.ts

---
+++ typescript/展开ts类型.ts
@@ -0,0 +1,5 @@
+type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+
+type ExpandRecursively<T> = T extends object
+  ? T extends infer O ? { [K in keyof O]: ExpandRecursively<O[K]> } : never
+  : T;
