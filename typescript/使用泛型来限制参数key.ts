3b35a4a1e86e84f897c8f8a1ef3e54807aa6c4d2
Liu <followliu@163.com>
2024-08-06 11:05:38 +0800

    Update 使用泛型来限制参数key.ts

--- typescript/使用泛型来限制参数key.ts
+++ typescript/使用泛型来限制参数key.ts
@@ -1,12 +1,15 @@
 /*
-https://www.typescriptlang.org/play/?ssl=2&ssc=3&pln=2&pc=45#code/C4TwDgpgBA0hIGcDyAzJwAWEBOAeAKlBAB7AQB2AJglAN4BQUUA2gNbxQCW5UCw23AOZQAPlHIBXALYAjHKN4hZAewA2AXQBcUCeVbllAd3L0AvgBooAVQB8UALx1GTFgAUuPdiGUprWt+pEpBTULF4+UPiBAPziEABu8tr4zK7qANzOppn0KLoAxsCcyjxSOIIQSDIAVhCFCARBZFQ0AEp1ytiUuHwC5IKWuvpG5DaWVk0hNHCIqOhYePjjNjYAFMo1AIzJlhvVAEzaVgCUyVAAZNZOTNgQwBLYPLRQAHRve5uWby97+1CmUAAhjRCJcrJlTPR6PkSnwgYCHFAytgKlVavVVs9AdpNv9LM8ZNo-qZjlCAPQAKgpjApUBh5DhgOx11pLiB2kkshwmSgtIBlwYvLZUEJ4mkcmwPL5NLJ0NhaggL1UykEqyZpPolOpQvw4GgAHJORL9VwaAZgECEAhOIJyICZKpoMBlFBQJAoIaEjh9S9VvsAMz7fak7hgCTAF7ABDEP37SwAFn9p0iWCCkEKEEorr1dOUZRoKGweagYCLkGwoA9gJNhgwnHyGFNUEodVUgNuWYW0BK2fd+pmyDQmBwuCxHPF3LxdBF465kvZYrn6X+Nn1MrlDItMhkiORqJqdSjmIXuIs09FcYXAf+xyAA
+https://www.typescriptlang.org/play/?#code/C4TwDgpgBA0hIGcDyAzJwAWEBOAeAUFFACpQQAewEAdgCYJQDehRUA2gNbxQCW1UCYNj4BzKAB8o1AK4BbAEY4JAkAoD2AGwC6ALijTqHamoDu1ANwsAvgBoWAVTsA+KAF4mLNgAVe-LiDUUKHtddi8tMkoaenZ-QJIIgH4pCAA3JT17by1LK0t8FAMAY2AeNX5ZHBEIJHkAKwgShFxSCio6BgAlRrVsWlxBYWoRG31DYzMnUftI9pi4RFR0LDxiaacnAAo1eoBGPTWoHbqAJkyASgOoADJgjyJsCGBpbH5GKAA6L+Pd0a+P44nKBWKAAQwYpFu9ly+HwRXKgjBoLcUEq2GqtQaTU271Bel2wNG73keiBVnOsIA9AAqamEalQeHURGgvH3BmsMF6GQKHCWKAMkG3ZgCzlQElSOSKbD8wX0ylwhGaCAfDRqESbVkU-A0umi4jgaAAch50qNvAYxmAYIQCB4ImooPkGmgwDUUFAkCgJrSOCNH02JwAzCcThS+GBpMAPsAEORAydRgAWIOXEhYSKQEoQWgew2MtSVBgobCFqBgUuQbCgb2g80mDA8IoYC1QWiNDSgx65lbQcp5r1GhbINCYHC4XHcqV8wlMcVT3kyrmSxfmYFOI3yxXM63yeQotEY+qNWM45cE2xziWJ5fB4HnIA
 **/
 
-type KeysOfOther<T extends {
-  [key in string | number | symbol]: unknown
-}, U> = {
-    [P in keyof U]: [P] extends [keyof T] ? never : T[P];
-  };
+type KeysOfOther<
+  T extends {
+    [key in string | number | symbol]: unknown;
+  },
+  U,
+> = {
+  [P in keyof U]: [P] extends [keyof T] ? never : U[P];
+};
 
 function mergeObjects<T extends Record<string, unknown>, U extends KeysOfOther<T, U>>(obj1: T, obj2: U): T & U {
   return { ...obj1, ...obj2 } as T & U;
